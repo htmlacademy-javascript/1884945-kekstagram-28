2bb72b985d53cd28c4b83a8d2ee2be0ffce97d65
Pandenimik <pandenimik@mail.ru>
2023-02-27 21:20:21 +0300

    Начал правки по замечаниям

--- js/main.js
+++ js/main.js
@@ -34,22 +34,18 @@ const getRandomInteger = (min, max) => {
   return Math.floor(result);
 };
 
-const createRandomIdFromRangeGenerator = (min, max) => {
-  const previousValues = [];
+const createsAnArrayOfConsecutiveNumbers = (min, max) =>
+  Array.from({ length: max }, (_, i) => i + min);
 
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= max - min + 1) {
-      // console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 };
+
+const createRandomIdArray = (min, max) =>
+  shuffleArray(createsAnArrayOfConsecutiveNumbers(min, max));
 
 const generatePhotoId = createRandomIdFromRangeGenerator(
   1,
@@ -97,3 +93,104 @@ const photosDescriptions = Array.from(
 photosDescriptions.concat([]);
 
 // console.log(photosDescriptions);
+
+// Старый код
+// const PHOTO_DESCRIPTIONS_COUNT = 25;
+
+// const MESSAGES = [
+//   'Всё отлично!',
+//   'В целом всё неплохо. Но не всё.',
+//   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
+//   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+//   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+//   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+// ];
+
+// const NAMES = [
+//   'Иван',
+//   'Хуан Себастьян',
+//   'Мария',
+//   'Кристоф',
+//   'Виктор',
+//   'Юлия',
+//   'Люпита',
+// ];
+// const DESCRIPTIONS = [
+//   'Моё любимое фото',
+//   'Опробовал новый фотоаппарат',
+//   'Фото через новый объектив',
+//   'Будничное фото',
+//   'Фото под настроение',
+// ];
+
+// const getRandomInteger = (min, max) => {
+//   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+//   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+
+//   return Math.floor(result);
+// };
+
+// const createRandomIdFromRangeGenerator = (min, max) => {
+//   const previousValues = [];
+
+//   return () => {
+//     let currentValue = getRandomInteger(min, max);
+//     if (previousValues.length >= max - min + 1) {
+//       // console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+//       return null;
+//     }
+//     while (previousValues.includes(currentValue)) {
+//       currentValue = getRandomInteger(min, max);
+//     }
+//     previousValues.push(currentValue);
+//     return currentValue;
+//   };
+// };
+
+// const generatePhotoId = createRandomIdFromRangeGenerator(
+//   1,
+//   PHOTO_DESCRIPTIONS_COUNT
+// );
+
+// const generateUrlId = createRandomIdFromRangeGenerator(
+//   1,
+//   PHOTO_DESCRIPTIONS_COUNT
+// );
+
+// const generateCommentId = createRandomIdFromRangeGenerator(1, 999);
+
+// const getRandomArrayElement = (elements) =>
+//   elements[getRandomInteger(0, elements.length - 1)];
+
+// const createPhotoDescription = () => ({
+//   id: generatePhotoId(),
+//   url: `photos/${generateUrlId()}.jpg`,
+//   description: getRandomArrayElement(DESCRIPTIONS),
+//   likes: getRandomInteger(15, 200),
+//   comments: [
+//     {
+//       id: generateCommentId(),
+//       avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+//       message: getRandomArrayElement(MESSAGES),
+//       name: getRandomArrayElement(NAMES),
+//     },
+//     {
+//       id: generateCommentId(),
+//       avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+//       message: getRandomArrayElement(MESSAGES),
+//       name: getRandomArrayElement(NAMES),
+//     },
+//   ],
+// });
+
+// const photosDescriptions = Array.from(
+//   { length: PHOTO_DESCRIPTIONS_COUNT },
+//   createPhotoDescription
+// );
+
+// // Пустая операция с массивом, чтобы eslint не выдавал ошибку:
+// // 'photosDescriptions' is assigned a value but never used.
+// photosDescriptions.concat([]);
+
+// // console.log(photosDescriptions);
