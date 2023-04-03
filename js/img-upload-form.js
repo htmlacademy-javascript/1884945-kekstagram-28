3108e59215cf107e1cc4a6a19315fbce06c0cdf1
Pandenimik <pandenimik@mail.ru>
2023-04-03 13:58:00 +0300

    доработал submit формы

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -16,12 +16,20 @@ const pristine = new Pristine(imgUploadForm, {
   errorTextParent: 'img-upload__field-wrapper',
 });
 
+const onSubmit = (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    sendData(evt);
+  }
+};
+
 const closeimgUploadOverlay = () => {
   imgUploadOverlay.classList.add('hidden');
   uploadFileInput.value = '';
   document.body.classList.remove('modal-open');
   imgUploadCancel.removeEventListener('click', closeimgUploadOverlay);
   document.removeEventListener('keydown', onDocumentKeydown);
+  imgUploadForm.removeEventListener('submit', onSubmit);
   hashTagsInput.removeEventListener('keydown', cancelEscKeydown);
   commentTextArea.removeEventListener('keydown', cancelEscKeydown);
   resetScale();
@@ -39,12 +47,7 @@ function onDocumentKeydown(evt) {
 
 const openimgUploadOverlay = () => {
   imgUploadOverlay.classList.remove('hidden');
-  imgUploadForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    if (pristine.validate()) {
-      sendData(evt);
-    }
-  });
+  imgUploadForm.addEventListener('submit', onSubmit);
   document.body.classList.add('modal-open');
   imgUploadCancel.addEventListener('click', closeimgUploadOverlay);
   document.addEventListener('keydown', onDocumentKeydown);
