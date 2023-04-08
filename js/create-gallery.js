c61ae1fb8786ec5db4d45ebbbf9890ee0d8455a5
Pandenimik <pandenimik@mail.ru>
2023-04-08 16:57:41 +0300

    Показ .img-filters после загрузки изображений

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -1,13 +1,14 @@
 import { getData } from './api.js';
 import { renderingThumbnails } from './rendering-thumbnails.js';
 import { openFullSizeThumbnail } from './full-size-thumbnail.js';
+import { showFilters } from './filters.js';
 
 const photoDescriptions = await getData().then((result) => result);
 const picturesContainer = document.querySelector('.pictures');
 
 const createGallery = () => {
   renderingThumbnails(photoDescriptions);
-
+  showFilters();
   picturesContainer.addEventListener('click', openFullSizeThumbnail);
 };
 
---
+++ js/filters.js
@@ -0,0 +1,6 @@
+const showFilters = () => {
+  const imgFilters = document.querySelector('.img-filters');
+  imgFilters.classList.remove('img-filters--inactive');
+};
+
+export { showFilters };
